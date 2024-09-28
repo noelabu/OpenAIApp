74c239367a60229702f9d7a65492adeaba24d33f
NoelaBunag <noela@egrowthpartners.com>
2024-09-29 00:00:39 +0800

    Build a simple UI for the chat history

--- frontend/openai-app/src/app/page.tsx
+++ frontend/openai-app/src/app/page.tsx
@@ -1,101 +1,67 @@
-import Image from "next/image";
+"use client"; 
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentChat, setCurrentChat] = useState('');
+  const [userChat, setUserChat] = useState([
+    {chat_type: "chat-end",
+      message: "Tell me something about stars."
+    },
+    {chat_type: "chat-start",
+      message: "Stars are in the sky."
+    }
+  ]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleChange = (e:any) => {
+    const chat = e.target.value;
+    setCurrentChat(chat);
+  };
+  
+  const handleSubmit = () => {
+    const chatHistory = [...userChat]
+    chatHistory.push({
+      chat_type:"chat-end",
+      message: currentChat
+    });
+    setUserChat(chatHistory);
+    setIsLoading(!isLoading);
+
+  };
+  return (
+    <section className='container mx-auto p-5 fixed inset-0'>
+      <div data-theme="retro" className="mockup-browser border bg-base-300 w-full h-full flex flex-col">
+        <div className="mockup-browser-toolbar">
+          <div className="bg-base-200 input">https://noelabu.github.io</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <div className='bg-base-200 p-5 pb-8 flex-grow overflow-auto'>
+          {userChat.map((chat, idx) => (
+            <div className={`chat ${chat?.chat_type}`}>
+              <div className="chat-bubble">
+                {chat?.message}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="w-full flex flex-col">
+          <form className="form-control m-5 items-center">
+            <div className="input-group w-full max-w-screen-2xl flex flex-col">
+              <textarea 
+                name="userText" 
+                className="textarea textarea-bordered bg-base-200 resize-none w-full max-w-screen-2xl"
+                onChange={handleChange}
+                placeholder="Ask me Anything.."
+              ></textarea>
+              <button className="btn btn-square btn-sm place-self-end mt-2" type="submit" onClick={handleSubmit} disabled={isLoading}>
+                { isLoading && (
+                  <span className="loading loading-dots loading-sm"></span>
+                )}
+              </button>
+            </div>
+          </form>
+          
+        </div>
+      </div>
+    </section>
+);
 }
